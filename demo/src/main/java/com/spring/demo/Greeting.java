5d70c7c888415c8018408f7c5b4f46d49c10c504
Sophia <sophia.gao.ch@gmail.com>
2016-08-14 18:20:54 +0930

    Adding Spring demo example

---
+++ demo/src/main/java/com/spring/demo/Greeting.java
@@ -0,0 +1,24 @@
+package com.spring.demo;
+
+/**
+ * Created by sophia on 11/08/16.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(final long id, final String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+}
